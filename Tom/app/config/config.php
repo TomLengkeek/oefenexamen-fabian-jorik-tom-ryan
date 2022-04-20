b97337582839b97ee651072cf72bad544b7b0bea
Tom Lengkeek <330713@student.mboutrecht.nl>
2022-04-20 10:32:20 +0200

    Update config.php

--- Tom/app/config/config.php
+++ Tom/app/config/config.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'oefenexm');
+define('DB_NAME', 'oefenexamen');
 
 //app root
 define('APPROOT', dirname(dirname(__FILE__)));
